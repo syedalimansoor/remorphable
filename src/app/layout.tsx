1b71d70782852f8364368a652ef92e8648b7a78c
Syed Ali Mansoor <alimansoor4812@gmail.com>
2025-07-25 22:56:52 -0600

    chore: initialize next project

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Roboto_Serif, Roboto_Mono } from "next/font/google";
+import "./globals.css";
+
+const robotoSerif = Roboto_Serif({
+  variable: "--font-roboto-serif",
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+  weight: ["400"],
+  style: ["normal", "italic"],
+});
+
+export const metadata: Metadata = {
+  title: "Remorphable",
+  description: "explore morphology by building your own words",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${robotoMono.variable} ${robotoSerif.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
