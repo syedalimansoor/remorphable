118a322ed507af749e087ccfe72c9aeaf464bf8f
Syed Ali Mansoor <alimansoor4812@gmail.com>
2025-08-02 22:02:21 -0600

    feat: add check badge

--- src/app/components/word-info.tsx
+++ src/app/components/word-info.tsx
@@ -1,10 +1,13 @@
+import { BadgeCheck } from "lucide-react";
+
 function WordInfo() {
   return (
     <div className="border border-foreground rounded-lg p-4 flex flex-col gap-4 max-w-128">
       <div className="flex flex-col gap-1">
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center gap-2">
           <h3 className="text-xl">destruction</h3>
-          <span className="text-sm italic">tis a real word</span>
+          <span className="text-sm italic ml-auto">tis a real word</span>
+          <BadgeCheck size={16} />
         </div>
         <span className="font-display text-sm">/dəˈstrəkSH(ə)n/</span>
       </div>
